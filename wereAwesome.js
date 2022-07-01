b69fa8ca6e2ebb1a586a9cc73d517c2058504df8
JesacaLin <JesacaLin@gmail.com>
2022-07-01 13:16:08 -0400

    initial commit

---
+++ wereAwesome.js
@@ -0,0 +1,13 @@
+// Write a function wereAwesome that takes you and your pair programming partner's name and returns the string "[your buddy's name] and [you] are awesome!"
+
+// If you are coding on your own, get creative! Instead of a pair programming partner, it could be the name of your friend, your pet, or even your rubber ducky!
+
+function wereAwesome(you, yourBuddy) {
+  return `${yourBuddy} and ${you} are awesome!`;
+}
+
+// Uncomment the lines below to test your function:
+console.log(wereAwesome("Dave", "Will")); // => "Will and Dave are awesome!"
+console.log(wereAwesome("Victoria", "Jenny")); // => "Jenny and Victoria are awesome!"
+console.log(wereAwesome("Chris", "Jac")); // => "Jac and Chris are awesome!"
+console.log(wereAwesome("Phillip", "Skyler")); // => "Skyler and Phillip are awesome!"
