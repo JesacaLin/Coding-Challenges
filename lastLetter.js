6fee1d8eddfa758513d5f4b13681a9277522c28b
JesacaLin <JesacaLin@gmail.com>
2022-07-01 13:23:43 -0400

    initial commit

---
+++ lastLetter.js
@@ -0,0 +1,14 @@
+// Create a function lastLetter that takes a word (string) and returns the last character/letter of that string.
+
+// Hint- Remember that each character/letter in a string has an index position that you can access with brackets- '[]'
+
+function lastLetter(word) {
+  //return word[word.length -1]
+  return word.at(-1);
+}
+
+// Uncomment the lines below to test your code
+console.log(lastLetter("hello")); //=> "o"
+console.log(lastLetter("goodbye!")); //=> "!"
+console.log(lastLetter("ZeltoiD")); //=> "D"
+console.log(lastLetter("I love Javascript")); //=> "t"
