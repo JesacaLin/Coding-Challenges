8b75d3ae2964ef49925db6d9feedae440c9c6ee4
JesacaLin <JesacaLin@gmail.com>
2022-07-01 12:44:46 -0400

    initial commit

---
+++ addTwo.js
@@ -0,0 +1,9 @@
+//Create a function addTwo that accepts a number, and returns the number plus 2.
+
+function addTwo(num) {
+  return num + 2;
+}
+addTwo(3);
+
+console.log(typeof addTwo);
+console.log(addTwo(10));
