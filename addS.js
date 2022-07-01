1b9107ab490124adda3653d035403d2f1a282ff9
JesacaLin <JesacaLin@gmail.com>
2022-07-01 12:54:48 -0400

    initial commit

---
+++ addS.js
@@ -0,0 +1,9 @@
+//Create a function addS that accepts a string, and returns the string with an "s" added to the end.
+
+function addS(string) {
+  return string + "s";
+}
+addS("tofu");
+// Uncomment these to check your work!
+console.log(typeof addS); // should log: 'function'
+console.log(addS("cat")); // should log: 'cats'
