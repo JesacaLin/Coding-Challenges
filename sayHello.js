fb657ad6e499ce89ffd7e63e7be9d7c45324d49b
JesacaLin <JesacaLin@gmail.com>
2022-07-01 13:07:49 -0400

    initial commit

---
+++ sayHello.js
@@ -0,0 +1,10 @@
+//Create a function sayHello that accepts a string argument and returns the string 'Hi, ' with the passed-in string added to the end.
+
+// ADD CODE HERE
+function sayHello(str) {
+  return `Hi, ${str}`;
+}
+
+// Uncomment these to check your work!
+console.log(sayHello("Mary")); // should log: 'Hi, Mary'
+console.log(sayHello("Haley")); // should log: 'Hi, Haley'
