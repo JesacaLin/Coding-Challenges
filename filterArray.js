7e6a38c08d1c420347707386f7bf22090fec938a
JesacaLin <JesacaLin@gmail.com>
2022-07-12 17:09:18 -0400

    initial commit

---
+++ filterArray.js
@@ -0,0 +1,26 @@
+// Add code to the functions func1 and func2 in the places marked "ADD CODE HERE" in order to achieve the desired console logs.
+
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [1, 2, 3, 4, 5];
+function func1(num) {
+  // ADD CODE HERE
+  if (num % 2 === 0) {
+    return num;
+  }
+}
+function func2(num) {
+  // ADD CODE HERE
+  if (num % 2 !== 0) {
+    return num;
+  }
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
