53067165771d0518479d3ddff6d4105767bcd9f4
JesacaLin <JesacaLin@gmail.com>
2022-07-14 22:22:30 -0400

    initial commit

---
+++ Closure-addByX.js
@@ -0,0 +1,30 @@
+// Now we are going to create a function addByX that returns a function that will add an input by x.
+
+// const addByTwo = addByX(2);
+// addByTwo(1); //should return 3
+// addByTwo(2); //should return 4
+// addByTwo(3); //should return 5
+
+// const addByThree = addByX(3);
+// addByThree(1); //should return 4
+// addByThree(2); //should return 5
+
+// const addByFour = addByX(4);
+// addByFour(4); //should return 8
+// addByFour(10); //should return 14
+
+// ADD CODE HERE
+function addByX(input) {
+  let sum = 0;
+  function helper(x) {
+    return (sum = input + x);
+  }
+  return helper;
+}
+
+const addByTwo = addByX(2);
+
+// Now call addByTwo with an input of 1 and log the output
+console.log(addByTwo(1));
+// Now call addByTwo with an input of 2 and log the output
+console.log(addByTwo(2));
