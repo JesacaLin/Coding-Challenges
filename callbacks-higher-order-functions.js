5ccc2625a7946fea6d2944699794136e36aa506b
JesacaLin <JesacaLin@gmail.com>
2022-10-23 15:58:30 -0400

    add callback .js and memoize.js

---
+++ callbacks-higher-order-functions.js
@@ -0,0 +1,19 @@
+LOOK; //ALL CSX Callbacks & Higher-order Functions challenges will go here
+
+//CHALLENGE: Pluralize
+
+//CHALLENGE: Map
+
+//CHALLENGE: Map #2
+
+//CHALLENGE: forEach
+
+//CHALLENGE: filterArray
+
+//CHALLENGE:
+
+//CHALLENGE:
+
+//CHALLENGE:
+
+//CHALLENGE:
---
+++ memoize.js
@@ -0,0 +1,5 @@
+/*
+Define a function memoize that takes one argument, a function, and returns a function.
+  
+When invoked, memoize creates an object that tracks calls to the returned function, where each input is associated with its output. Every subsequent call to that returned function with the same argument will return the output directly from the object, instead of invoking the original function again.
+*/
