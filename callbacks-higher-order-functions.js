6d338ed87bcb81a0b73350df899645e833e63bf2
JesacaLin <JesacaLin@gmail.com>
2022-12-10 20:54:28 -0500

    eitherCallback

--- callbacks-higher-order-functions.js
+++ callbacks-higher-order-functions.js
@@ -145,7 +145,31 @@ const integerSquareRoot = (n) => Math.sqrt(n) % 1 === 0;
 const over100 = (n) => n > 100;
 console.log(eitherFilter(arrOfNums, integerSquareRoot, over100)); // should log: [105, 9]
 
-LOOK; //CHALLENGE:
+LOOK; //CHALLENGE: eitherCallback
+// Add code to the function eitherCallback in the place marked "ADD CODE HERE" in order to achieve the desired console logs. Notice that the lines of code testing your work are using functions and an array from previous challenges. The result of using eitherCallback to combine two callbacks into one callback and then passing that one callback into filterArray should match the results of simply passing the two callbacks into eitherFilter in the previous challenge.
+
+function eitherCallback(callback1, callback2) {
+  //ADD CODE HERE
+  return (element) => {
+    return callback1(element) || callback2(element);
+  };
+}
+
+// Uncomment these to check your work!
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i], i, array)) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [10, 35, 105, 9];
+const integerSquareRoot = (n) => Math.sqrt(n) % 1 === 0;
+const over100 = (n) => n > 100;
+const intSqRtOrOver100 = eitherCallback(integerSquareRoot, over100);
+console.log(filterArray(arrOfNums, intSqRtOrOver100)); // should log: [105, 9]
+
+//In this implementation, the eitherCallback() function takes two callback functions as arguments and returns a new callback function that combines the behavior of the two input functions. The new callback function returns a truthy value if either of the input callback functions returns a truthy value
 
 LOOK; //CHALLENGE:
 
