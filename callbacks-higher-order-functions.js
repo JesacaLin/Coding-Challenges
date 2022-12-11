387e64e2461393fa68f40b2a0889debdaf9f1a3c
JesacaLin <JesacaLin@gmail.com>
2022-12-10 20:00:49 -0500

    filterArray

--- callbacks-higher-order-functions.js
+++ callbacks-higher-order-functions.js
@@ -99,7 +99,31 @@ forEach(["a", "b", "c"], (i) => console.log(i)); // should log: 'a', 'b', 'c'
 console.log(typeof map); // should log: 'function'
 console.log(map([3, 4, 5], (n) => n - 2)); // should log: [1, 2, 3]
 
-LOOK; //CHALLENGE: forEach
+LOOK; //CHALLENGE: filterArray
+// Add code to the functions func1 and func2 in the places marked "ADD CODE HERE" in order to achieve the desired console logs.
+
+function filterArray(array, callback) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i += 1) {
+    if (callback(array[i])) newArray.push(array[i]);
+  }
+  return newArray;
+}
+const arrOfNums = [1, 2, 3, 4, 5];
+function func1(num) {
+  if (num % 2 === 0) {
+    return num;
+  }
+}
+function func2(num) {
+  if (num % 2 !== 0) {
+    return num;
+  }
+}
+
+// Uncomment these to check your work!
+console.log(filterArray(arrOfNums, func1)); // should log: [2, 4]
+console.log(filterArray(arrOfNums, func2)); // should log: [1, 3, 5]
 
 LOOK; //CHALLENGE: filterArray
 
