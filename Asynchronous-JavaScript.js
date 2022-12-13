92175c537561051189c9b829e4d7b739045c2a54
JesacaLin <JesacaLin@gmail.com>
2022-12-12 23:09:09 -0500

    setTimeout

---
+++ Asynchronous-JavaScript.js
@@ -0,0 +1,15 @@
+LOOK; //CHALLENGE: setTimeout
+
+function setCode() {
+  console.log("I am at the beginning of the code");
+  setTimeout(() => {
+    console.log(`I am in the setTimeout callback function.`);
+  }, 600);
+  console.log("I am at the end of the code");
+}
+
+setCode();
+
+LOOK; //CHALLENGE:
+
+LOOK; //CHALLENGE:
