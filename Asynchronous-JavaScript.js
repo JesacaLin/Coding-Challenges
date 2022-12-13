251ce2ce7d24df67e8c7720d049341bdd13edf20
JesacaLin <JesacaLin@gmail.com>
2022-12-13 00:02:02 -0500

    forEach

--- Asynchronous-JavaScript.js
+++ Asynchronous-JavaScript.js
@@ -10,6 +10,21 @@ function setCode() {
 
 setCode();
 
-LOOK; //CHALLENGE:
+LOOK; //CHALLENGE: forEach
+function forEach(arr, cb) {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i);
+  }
+}
+
+const delays = [200, 500, 0, 350];
+
+function delayLog(delayTime, i) {
+  setTimeout(() => {
+    console.log(`printing element ${i}`);
+  }, delayTime);
+}
+
+forEach(delays, delayLog);
 
 LOOK; //CHALLENGE:
