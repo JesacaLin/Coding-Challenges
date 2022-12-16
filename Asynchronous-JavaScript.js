9327a22dd7dad6879449797a50a20dfabeacab2a
JesacaLin <JesacaLin@gmail.com>
2022-12-16 18:54:30 -0500

    intersection

--- Asynchronous-JavaScript.js
+++ Asynchronous-JavaScript.js
@@ -27,4 +27,40 @@ function delayLog(delayTime, i) {
 
 forEach(delays, delayLog);
 
-LOOK; //CHALLENGE:
+LOOK; //CHALLENGE: intersection
+
+//Need to study this one
+//input: array of arrays
+//output: one array
+function intersection(arrays) {
+  //accumulator = starts as one of our arrays
+  let acc = arrays.pop();
+  //loop through array of arrays
+  while (arrays.length) {
+    const current = arrays.pop();
+    const newArr = [];
+
+    //compare current array with our accumulator
+    //for each element in current
+    //if that element exists in accumulator
+    //push that element into a new array
+    current.forEach((element) => {
+      if (acc.inclues(element)) newArr.push(element);
+    });
+    //accumlator reassigned to new array
+    acc = newArr;
+  }
+  // return our accumulator
+  return acc;
+}
+
+const arr1 = [5, 10, 15, 20];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [1, 10, 15, 5, 20];
+console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
+
+// function intersection(arrays) {
+//   return arrays.reduce((intersection, array) => {
+//     return intersection.filter(element => array.includes(element));
+//   });
+// }
