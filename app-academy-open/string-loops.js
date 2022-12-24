ba2ff66d37d36e3a4fc971018f1078350625dbfd
JesacaLin <JesacaLin@gmail.com>
2022-12-24 15:53:26 -0500

    big latin research

--- app-academy-open/string-loops.js
+++ app-academy-open/string-loops.js
@@ -28,7 +28,42 @@ console.log(countVowels("bootcamp")); // => 3
 console.log(countVowels("apple")); // => 2
 console.log(countVowels("pizza")); // => 2
 
-//LOOK --
+//LOOK --Pig Latin Slice Research
+// Pig Latin is a fun take on the English language where you move any consonant cluster from the start of the word to the end of the word; when words begin on a vowel, you simply add "-yay". Vowels are "aeiou".
+
+// Write a function pigLatinWord that takes in a word string and translates the word into Pig Latin. For this problem use the String.slice() method. The slice() method extracts a section of a string and returns it as a new string, without modifying the original string.
+
+// Hint: Remember the String.includes method!
+
+// So the two rules for our version of Pig Latin are:
+
+// 1. For words that start with a vowel, add 'yay' to the end of the word.
+// 2. For words that start with a non-vowel, move all letters that come
+// **before the first vowel** to the **end of the word** then add 'ay'
+//input: string;
+//output: new string;
+//declare a string of vowels
+//initialize a for loop to check elments in each word.
+//declare variables to store mutated strings before putting it all together again.
+
+function pigLatinWord(word) {
+  word = word.toLowerCase();
+  const vowels = "aeiou".toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    if (vowels.includes(word[0])) {
+      return `${word}yay`;
+    } else if (vowels.includes(word[i])) {
+      let beforeSplit = word.slice(0, i);
+      let afterSplit = word.slice(i);
+      return `${afterSplit}${beforeSplit}ay`;
+    }
+  }
+}
+
+console.log(pigLatinWord("apple")); //=> "appleyay"
+console.log(pigLatinWord("eat")); //=> "eatyay"
+console.log(pigLatinWord("banana")); //=> "ananabay"
+console.log(pigLatinWord("trash")); //=> "ashtray"
 
 //LOOK --
 
