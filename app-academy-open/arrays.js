431c9a97c1f8358e932467ce1f3767b6e96d19bc
JesacaLin <JesacaLin@gmail.com>
2022-12-28 18:22:24 -0500

    My Includes

--- app-academy-open/arrays.js
+++ app-academy-open/arrays.js
@@ -93,4 +93,24 @@ console.log(threeIncreasing([2, 7, 8, 9])); // true
 console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6])); // false
 console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8])); // false
 
-//LOOK -->
+//LOOK -->My Includes
+// Write a function myIncludes(arr, target) that accepts an array and a target value as args. The function should return a boolean indicating whether the target is found in the array. Solve this without Array.includes or Array.indexOf.
+
+//input: arr, num
+//output: boolean
+
+//check if arr has the num value
+//return true/false
+
+const myIncludes = (arr, target) => {
+  if (arr.includes(target)) {
+    return true;
+  }
+  return false;
+};
+
+console.log(myIncludes(["a", "b", "c", "e"], "c")); // true
+console.log(myIncludes(["a", "b", "c", "e"], "a")); // true
+console.log(myIncludes(["a", "b", "c", "e"], "z")); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
