dfb24a9f8631661b4c79738477e9fb6cd188d4c5
JesacaLin <JesacaLin@gmail.com>
2022-12-28 16:18:43 -0500

    Three Increasing

--- app-academy-open/arrays.js
+++ app-academy-open/arrays.js
@@ -59,6 +59,38 @@ function combineArray(array1, array2) {
 console.log(combineArray([1, 2], [3, 4])); // => [1, 2, 3, 4]
 console.log(combineArray([17, 5], [6, 7])); // => [17, 5, 6, 7]
 
-//LOOK -->
+//LOOK -->Three Increasing
+// Write a function threeIncreasing that accepts an array of numbers as an argument. The function should return a boolean indicating whether or not the array contains three consecutive numbers in consecutive increasing order, like 7, 8, 9.
+
+//input: array
+//output: boolean
+//check if array contains three consecutive numbers
+//cover edge case of lengths less than 3.
+//initialize a loop
+//create conditional to check if 2nd element is bigger than first element by 1.
+//AND if 3rd element is bigger than second element by 2.
+//return true or false
+
+function threeIncreasing(arr) {
+  for (let i = 0; i < arr.length - 2; i++) {
+    if (arr.length < 3) return false;
+    // let currentNum = arr[i]
+    // console.log(`currentNum: ${currentNum}`)
+    // let nextNum = arr[i+1]
+    // console.log(`nextNum: ${nextNum}`)
+
+    if (arr[i + 1] === arr[i] + 1 && arr[i + 2] === arr[i] + 2) {
+      return true;
+    }
+  }
+  //placed outside the for loop because it would allow the loop to continue checking the elements.
+  //if placed inside for loop, it would stop the loop the first time it doesn't satisfy if.
+  return false;
+}
+
+console.log(threeIncreasing([3, 2, 11, 12, 13, 2, 4])); // true
+console.log(threeIncreasing([2, 7, 8, 9])); // true
+console.log(threeIncreasing([7, 2, 4, 5, 2, 1, 6])); // false
+console.log(threeIncreasing([1, 2, 4, 5, 2, 7, 8])); // false
 
 //LOOK -->
