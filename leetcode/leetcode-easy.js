13a8a18ad79cc79a9e411fcf4e7913aa95971b77
JesacaLin <JesacaLin@gmail.com>
2023-01-02 02:26:05 -0500

    two sum

--- leetcode/leetcode-easy.js
+++ leetcode/leetcode-easy.js
@@ -37,3 +37,72 @@ var detectCapitalUse = function (word) {
   }
   return true;
 };
+
+//LOOK -------------->Two Sum
+// Easy
+// 41.7K
+// 1.4K
+// Companies
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+//input: array and target num
+//output: array, two indices
+
+//establish empty array
+//iterate through the values
+//establish current value
+//establish inner loop
+//add inner loop val to current value
+//if adds up to target, push to new array
+
+var twoSum = function (nums, target) {
+  let newArray = [];
+  for (let i = 0; i < nums.length; i++) {
+    let currentVal = nums[i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (currentVal + nums[j] === target) {
+        newArray.push(i, j);
+      }
+    }
+  }
+
+  return newArray;
+};
+console.log(twoSum([2, 7, 11, 15], 9)); //[0, 1]
+console.log(twoSum([2, 7, 11, 15], 9)); //[0, 1]
+console.log(twoSum([3, 2, 4], 6)); //[1, 2]
+console.log(twoSum([3, 3], 6)); //[0, 1]
+
+//LOOK -------------->
+//LOOK -------------->
+//LOOK -------------->
+//LOOK -------------->
+
+//LOOK -------------->
+//LOOK -------------->
+//LOOK -------------->
