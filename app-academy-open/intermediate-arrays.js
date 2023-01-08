97be035f29be671fb15edef2ef3072bbf3e560dc
JesacaLin <JesacaLin@gmail.com>
2023-01-08 18:13:07 -0500

    remove from array

--- app-academy-open/intermediate-arrays.js
+++ app-academy-open/intermediate-arrays.js
@@ -518,3 +518,42 @@ function summationSequence(start, length) {
 
 console.log(summationSequence(3, 4)); // [3, 6, 21, 231]
 console.log(summationSequence(5, 3)); // [5, 15, 120]
+
+//TODO -------------->Remove From Array Research
+//input: string, array
+//output: mutated array
+
+function removeFromArray(location, arr) {
+  if (location === "FRONT") {
+    arr.shift();
+  } else if (location === "BACK") {
+    arr.pop();
+  } else {
+    console.log("ERROR");
+  }
+}
+
+testArray = [0, 1, 2, 3, 4];
+
+removeFromArray("FRONT", testArray);
+console.log(testArray); // [1,2,3,4]
+
+removeFromArray("BACK", testArray);
+console.log(testArray); // [1,2,3]
+
+removeFromArray("MIDDLE", 4, testArray); // "ERROR"
+console.log(testArray); // [1,2,3]
+
+//TODO -------------->
+
+//TODO -------------->
+
+//TODO -------------->
+
+//TODO -------------->
+
+//TODO -------------->
+
+//TODO -------------->
+
+//TODO -------------->
