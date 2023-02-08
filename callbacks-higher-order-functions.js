4923cc2902cf0316b889583cdca2d5e7b36d981e
JesacaLin <JesacaLin@gmail.com>
2023-02-08 14:06:49 -0500

    union

--- callbacks-higher-order-functions.js
+++ callbacks-higher-order-functions.js
@@ -228,7 +228,32 @@ console.log(intersection([arr1, arr2, arr3])); // should log: [5, 15]
 //   });
 // }
 
-LOOK; //CHALLENGE:
+LOOK; //CHALLENGE: UNION
+/*Construct a function union that takes an input array of arrays, compares each array, and returns a new flat array that contains all elements. If there are duplicate elements, only add it once to the new array. Preserve the order of the elements starting from the first element of the first input array. BONUS - Use reduce!*/
+
+function union(arr) {
+  //use reduce to iterate through the outter arrays.
+  //initialize an empty array to be our output.
+  const findUnique = arr.reduce((acc, curr) => {
+    //use forEach to compare ccurrent value with inner array.
+    curr.forEach((value) => {
+      //set conditional. If value is not present int he ouput array, add it.
+      if (!acc.includes(value)) {
+        acc.push(value);
+      }
+    });
+    //return output array
+    return acc;
+  }, []);
+  //return the result of calling reduce.
+  return findUnique;
+}
+
+// Uncomment these to check your work!
+const arr1 = [5, 10, 15];
+const arr2 = [15, 88, 1, 5, 7];
+const arr3 = [100, 15, 10, 1, 5];
+console.log(union([arr1, arr2, arr3])); // should log: [5, 10, 15, 88, 1, 7, 100]
 
 LOOK; //CHALLENGE:
 
