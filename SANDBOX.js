a8fd3ab17ff7645427c1ef30650ce0f25ad93880
JesacaLin <JesacaLin@gmail.com>
2023-02-14 12:37:33 -0500

    power of two

--- SANDBOX.js
+++ SANDBOX.js
@@ -78,9 +78,6 @@ The fibonacci sequence is a sequence of numbers whose first and second elements 
 function findFibo(n) {
   const array = [0, 1]; //O(1) constant time
   for (let i = 2; i < n; i++) {
-    // let last = array[i - 1]
-    // let secondToLast = array[i - 2]
-    // array.push(last + secondToLast)
     array[i] = array[i - 1] + array[i - 2]; //O(n) Linear
   }
   return array; //0(1) constant time
@@ -88,5 +85,69 @@ function findFibo(n) {
 
 //Big-O = O(n): Linear
 
-console.log(findFibo(2));
-console.log(findFibo(7));
+// console.log(findFibo(2));
+// console.log(findFibo(7));
+
+//TODO-->Factorial
+
+//given an integer 'n', find the factorial of that integer
+
+//what is a factorial? - a non-negative integer is the product of all positivie integers less than or equal to "n"
+
+//have a counter variable to store changing values
+//initialize a reverse loop multiple n times
+//retrn the counter variable
+
+function factorials(n) {
+  let counter = n;
+  for (let i = n; i > 1; i--) {
+    counter *= i - 1;
+  }
+  return counter;
+}
+//Big O = O(n)
+// console.log(factorials(5))
+
+//TODO --> Give a natural number 'n', determine if the number is prime or not
+
+//set condition if it's divisible by 2?
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// console.log(isPrime(5));
+// console.log(isPrime(4));
+// console.log(isPrime(15));
+
+//TODO --> Give a positive ineger 'n', determine if the number is a power of 2 or not.
+
+//what is a power of 2?
+//an integer is a power of two if there exists an integer 'x' such that 'n' === 2 to power of x.
+//test that integer is positive
+//initial a while loop?
+//use if statement to test if number is power of two
+
+function isPowerOfTwo(num) {
+  if (num <= 0) return false;
+  for (let i = 0; i < num; i++) {
+    if (num === 2 ** i) {
+      return true;
+    }
+  }
+  return false;
+}
+
+//Big O = O(log n)
+
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(8));
+console.log(isPowerOfTwo(5));
+console.log(isPowerOfTwo(1));
