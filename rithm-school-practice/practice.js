1201365667b8ae0120a388fee8cd54e6cf3d72b4
JesacaLin <JesacaLin@gmail.com>
2023-03-28 22:18:27 -0400

    rithm objs practice

--- rithm-school-practice/practice.js
+++ rithm-school-practice/practice.js
@@ -45,3 +45,42 @@ let people = ["Greg", "Mary", "Devon", "James"];
 // Create a new variable called withBob and set it equal to the people array concatenated with the string of "Bob".
 // let withBob = people.concat("Bob");
 // console.log(withBob);
+
+let programming = {
+  languages: ["JavaScript", "Python", "Ruby"],
+  isChallenging: true,
+  isRewarding: true,
+  difficulty: 8,
+  jokes:
+    "http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke",
+};
+// Write the command to add the language "Go" to the end of the languages array.
+programming.languages.push("Go");
+// console.log(programming);
+
+// Change the difficulty to the value of 7.
+programming.difficulty = 7;
+
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+delete programming.jokes;
+
+// Write the command to add a new key called isFun and a value of true to the programming object.
+programming["isFun"] = true;
+
+// Using a loop, iterate through the languages array and console.log all of the languages.
+let langs = programming.languages;
+for (let i = 0; i < langs.length; i++) {
+  console.log(langs[i]);
+}
+
+// Using a loop, console.log all of the keys in the programming object.
+for (let key in programming) {
+  console.log(key);
+}
+
+// Using a loop, console.log all of the values in the programming object.
+for (let key in programming) {
+  console.log(programming[key]);
+}
+
+console.log(programming);
