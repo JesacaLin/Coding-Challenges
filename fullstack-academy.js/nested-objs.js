6ac465834b43861ffada623320607bd714fa27cf
JesacaLin <JesacaLin@gmail.com>
2023-03-31 11:43:42 -0400

    nested obj part 1

--- fullstack-academy.js/nested-objs.js
+++ fullstack-academy.js/nested-objs.js
@@ -119,3 +119,40 @@
     isYoungest: true
 }
 */
+
+// Given the following nested object:
+
+let nestedData = {
+  innerData: {
+    order: ["first", "second", "third"],
+    snacks: ["chips", "fruit", "crackers"],
+    numberData: {
+      primeNumbers: [2, 3, 5, 7, 11],
+      fibonnaci: [1, 1, 2, 3, 5, 8, 13],
+    },
+    addSnack: function (snack) {
+      nestedData.innerData.snacks.push(snack);
+      return nestedData.innerData.snacks;
+    },
+  },
+};
+
+// Using a for loop, console.log all of the numbers in the primeNumbers array.
+const primeLoop = nestedData.innerData.numberData.primeNumbers;
+for (let i = 0; i < primeLoop.length; i++) {
+  console.log(primeLoop[i]);
+}
+
+// Using a for loop, console.log all of the even Fibonnaci numbers.
+const fibLoop = nestedData.innerData.numberData.fibonnaci;
+const fibFilter = fibLoop.filter((num) => {
+  return num % 2 === 0;
+});
+console.log(fibFilter);
+
+// Console.log the value "second" inside the order array.
+console.log(nestedData.innerData.order[1]);
+
+// Invoke the addSnack function and add the snack "chocolate".
+nestedData.innerData.addSnack("chocolate");
+console.log(nestedData);
