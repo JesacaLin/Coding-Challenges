5c62e592a8c5e1773f6db286b540efaebe33dfc5
JesacaLin <JesacaLin@gmail.com>
2023-05-07 16:07:48 -0400

    OOP personFromPersonStore

--- CSX/OOP.js
+++ CSX/OOP.js
@@ -1,26 +1,46 @@
 //-->MAKE PERSON
 
-function makePerson(name, age) {
-  const obj = {};
-  obj.name = name;
-  obj.age = age;
-  return obj;
+// function makePerson(name, age) {
+//   const obj = {};
+//   obj.name = name;
+//   obj.age = age;
+//   return obj;
+// }
+
+// const vicky = makePerson("Vicky", 24);
+
+// // Uncomment these lines to check your work!
+// console.log(vicky.name); // -> Logs 'Vicky'
+// console.log(vicky.age); // -> Logs 24
+
+// //personStore
+// const personStore = {
+// 	greet: function(){
+//   console.log('hello')
+// }
+// };
+
+// // Uncomment this line to check your work!
+// personStore.greet(); // -> Logs 'hello'
+
+//personFromPersonStore
+const personStore = {
+  greet: function () {
+    console.log("hello");
+  },
+};
+
+function personFromPersonStore(name, age) {
+  // add code here
+  const newObj = Object.create(personStore);
+  newObj.name = name;
+  newObj.age = age;
+  return newObj;
 }
 
-const vicky = makePerson("Vicky", 24);
+const sandra = personFromPersonStore("Sandra", 26);
 
 // Uncomment these lines to check your work!
-console.log(vicky.name); // -> Logs 'Vicky'
-console.log(vicky.age); // -> Logs 24
-
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
-//LOOK--->
+console.log(sandra.name); // -> Logs 'Sandra'
+console.log(sandra.age); // -> Logs 26
+sandra.greet(); // -> Logs 'hello'
