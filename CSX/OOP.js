d11c3ae5e4fa2fa63b0fb2641e40fe7303e04104
JesacaLin <JesacaLin@gmail.com>
2023-05-07 15:54:09 -0400

    OOP-makePerson

---
+++ CSX/OOP.js
@@ -0,0 +1,26 @@
+//-->MAKE PERSON
+
+function makePerson(name, age) {
+  const obj = {};
+  obj.name = name;
+  obj.age = age;
+  return obj;
+}
+
+const vicky = makePerson("Vicky", 24);
+
+// Uncomment these lines to check your work!
+console.log(vicky.name); // -> Logs 'Vicky'
+console.log(vicky.age); // -> Logs 24
+
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
+//LOOK--->
